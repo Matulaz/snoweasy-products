00afb666da7117dd7149358b292fdc65f1317f32
matulaz <matiaszarate.psi@gmail.com>
2023-03-20 13:33:23 -0300

    deploy the site w/ netlify

--- src/components/rental.jsx
+++ src/components/rental.jsx
@@ -5,7 +5,7 @@ function Rental()  {
 
     const rental = {
         origen:["Buenos Aires", "Córdoba", "Rosario", "Bariloche", "Malargue", "San Rafael", "San Juan","San Martin de Los Andes", "Los Molles"],
-        destinos: ["Las Leñas", "Chapelco", "Cerro Castor"],
+        destinos: ["Las Leñas", "Chapelco", "Cerro Castor", "Bariloche"],
         pax:["Alta Gama", "Sport", "Menor"],
         tipoEquipos:["Equipo Ski", "Equipo Snowboard"],
         cant:["x 1 Dia", "x 2 Dias", "x 3 Dias", "x 4 Dias", "x 5 Dias", "x 6 Dias", "x 7 Dias", "x 8 Dias", "x 9 Dias", "x 10 Dias"],
@@ -51,7 +51,7 @@ function Rental()  {
             </select>
             </label>
 
-            <label for="rentalDestinos">Destino
+            <label for="rentalDestinos">Centro de Ski
             <select>
                 {recorrerDestinos}
             </select>
